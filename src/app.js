431c81ce2d81ec3b97a01afda3e5813713f2f59e
Soham-59 <162586540+Soham-59@users.noreply.github.com>
2025-11-05 14:34:17 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import postRoutes from "./routes/postRoutes.js";
-import aiRoutes from "./routes/aiRoutes.js";
+
 dotenv.config();
 const app = express();
 
